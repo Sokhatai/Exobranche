c033d6a17a12b6a7ed1957e153b986f476df1fe5
Sokhatai <auguin.corentin@orange.fr>
2024-10-08 15:42:54 +0200

    PohmPohmPOHM

--- exo.js
+++ exo.js
@@ -5,3 +5,7 @@ console.log("Negapi est meilleur que Pikachu");
 //deuxième commit
 
 console.log("Pikachu reste un bon pokemon");
+
+//Commit 1ere branche (Pohm)
+
+console.log("Pohm est le meilleur pikachu like de toute façon");
