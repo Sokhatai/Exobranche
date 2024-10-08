06162176be377bff186fa54d8cc43027c4119821
Sokhatai <auguin.corentin@orange.fr>
2024-10-08 15:39:56 +0200

    third commit

--- exo.js
+++ exo.js
@@ -5,3 +5,8 @@ console.log("Negapi est meilleur que Pikachu");
 //deuxième commit
 
 console.log("Pikachu reste un bon pokemon");
+
+//troisième commit (après la première branche)
+
+console.log("Vous oubliez Pichu");
+
